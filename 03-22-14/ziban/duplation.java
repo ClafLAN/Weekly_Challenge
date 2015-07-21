460ad528074cee3440e086c46942712cb001d287
ziban <csirmah@gmail.com>
2015-07-21 15:18:06 -0400

    Update duplation.java
    Just some cleaning of old code

--- 03-22-14/ziban/duplation.java
+++ 03-22-14/ziban/duplation.java
@@ -10,15 +10,15 @@ public class duplation {
 	 * returns -1 if there is an error
 	 */
 
-	public int checkEven(int x){
+	public int checkEven(int x) {
 
-		int ifeven=-1;
+		int ifeven = -1;
 
-		if(x %2 ==0){
-			ifeven=1;
+		if(x %2 == 0) {
+			ifeven = 1;
 		}
-		else if(x%2!=0){
-			ifeven=0;
+		else if(x%2! = 0 ) {
+			ifeven = 0;
 		}
 
 		return ifeven;
@@ -29,24 +29,24 @@ public class duplation {
 	 * Method to do the multiplication of two integers using duplation(Ethiopian Multiplication)
 	 * Algorithm described at http://rosettacode.org/wiki/Ethiopian_multiplication
 	 */
-	public void duplicationMultiplication(int a, int b){
+	public void duplicationMultiplication(int a, int b) {
 
-		int leftRow=a;
-		int rightRow=b;
-		int Total=0;
+		int leftRow = a;
+		int rightRow = b;
+		int Total = 0;
 
-		while(leftRow>=1){			
+		while(leftRow >= 1){			
 			System.out.print("Right Column\t|\tLeft Column\n");
-			System.out.println(leftRow+"\t\t|\t"+rightRow+"\n");
+			System.out.println(leftRow + "\t\t|\t" +r ightRow + "\n");
 			
 			//Adds up if the number on the left is not even
-			if(checkEven(leftRow)!=1){
-				Total=Total+rightRow;
+			if(checkEven(leftRow) != 1) {
+				Total = Total + rightRow;
 			}
 			
 			//Divides numbers on the left by two and multiplies number on the right by two
-			leftRow= (int)leftRow/2;
-			rightRow=rightRow*2;
+			leftRow = (int)leftRow / 2;
+			rightRow = rightRow * 2;
 		}
 		
 		System.out.println("---------The total is "+ Total +"--------------");
@@ -60,9 +60,9 @@ public class duplation {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Please enter the first number");
-		int first=input.nextInt();
+		int first = input.nextInt();
 		System.out.println("Please enter the second number");
-		int second= input.nextInt();
+		int second = input.nextInt();
 		
 		myDuplation.duplicationMultiplication(first, second);
 		
